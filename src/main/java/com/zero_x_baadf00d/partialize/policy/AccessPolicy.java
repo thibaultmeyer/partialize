1a01271320231879a873c99a1ed7aa5efbf2e1a5
Thibault Meyer <meyer.thibault@gmail.com>
2016-12-27 18:20:19 +0100

    Upgrade license year (2017)
    Signed-off-by: Thibault Meyer <meyer.thibault@gmail.com>

--- src/main/java/com/zero_x_baadf00d/partialize/policy/AccessPolicy.java
+++ src/main/java/com/zero_x_baadf00d/partialize/policy/AccessPolicy.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2016 Thibault Meyer
+ * Copyright (c) 2016 - 2017 Thibault Meyer
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
